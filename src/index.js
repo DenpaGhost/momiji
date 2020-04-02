c950a471f1f8266a6591a586cb90eab9e0af50ac
おかもと <az.kinosaki@gmail.com>
2020-04-02 18:06:28 +0900

    [update]古いのを削除

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import '~/resource/style/app.scss';
 
 import Vue from "vue";
 import VueRouter from 'vue-router';
-import Base from "~/src/components/Base";
 import route from "~/src/routes/route";
 
 Vue.use(VueRouter);
-Vue.component('v-base', Base);
 
 const router = new VueRouter(route);
 
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,5 +1,3 @@
-import Base from "~/src/components/Base.vue";
-import Gallery from "~/src/pages/Gallery.vue";
 import Preview from "~/src/pages/Preview.vue";
 import SwipeableBoxPreview from "~/src/pages/SwipeableBoxPreview.vue";
 
@@ -9,15 +7,6 @@ export default {
             path: '/',
             component: Preview,
             name: 'base'
-        },
-        {
-            path: '/gallery/:index',
-            name: 'gallery',
-            component: Gallery
-        },
-        {
-            path: '/preview',
-            component: Base
         },
         {
             path: '/box-preview/:index',
