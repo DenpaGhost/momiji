2d3fbc53e0d2b7bad3ccad134c97919af86d9cf7
おかもと <az.kinosaki@gmail.com>
2020-05-07 15:16:36 +0900

    [add]ピンチ操作イベント

--- src/components/v2/MomijiTouchHandler.vue
+++ src/components/v2/MomijiTouchHandler.vue
@@ -17,32 +17,74 @@
      */
     import {Component, Vue} from "vue-property-decorator";
     import MomijiSwipeEvent from "~/src/models/events/MomijiSwipeEvent";
+    import MomijiPinchEvent from "~/src/models/events/MomijiPinchEvent";
 
     @Component
     export default class MomijiTouchHandler extends Vue {
         swipeEvent?: MomijiSwipeEvent;
+        pinchEvent?: MomijiPinchEvent;
 
         handleTouchStart(event: TouchEvent) {
             if (event.touches.length == 1) {
-                this.swipeEvent = new MomijiSwipeEvent(event.touches[0]);
-                this.$emit('swipestart', this.swipeEvent);
+                this.attachSwipeEvent(event.touches[0]);
+            } else if (event.touches.length == 2) {
+                this.detachSwipeEvent();
+                this.attachPinchEvent(event.touches[0], event.touches[1]);
             }
         }
 
         handleTouchMove(event: TouchEvent) {
             if (event.touches.length == 1) {
-                this.swipeEvent =
-                    this.swipeEvent?.move(event.touches[0]) ?? new MomijiSwipeEvent(event.touches[0]);
-
-                this.$emit('swipemove', this.swipeEvent);
+                this.updateSwipeEvent(event.touches[0]);
+            } else if (event.touches.length == 2) {
+                this.updatePinchEvent(event.touches[0], event.touches[1]);
             }
         }
 
         handleTouchEnd(event: TouchEvent) {
             if (event.touches.length <= 0) {
-                this.swipeEvent = undefined;
-                this.$emit('swipeend');
+                this.detachSwipeEvent();
+            } else if (event.touches.length <= 1) {
+                this.detachPinchEvent();
             }
+        }
+
+        /*
+         * methods
+         */
+
+        attachSwipeEvent(touch: Touch) {
+            this.swipeEvent = new MomijiSwipeEvent(touch);
+            this.$emit('swipestart', this.swipeEvent);
+        }
+
+        updateSwipeEvent(touch: Touch) {
+            if (!!this.swipeEvent) {
+                this.swipeEvent.move(touch);
+                this.$emit('swipemove', this.swipeEvent);
+            }
+        }
+
+        detachSwipeEvent() {
+            this.swipeEvent = undefined;
+            this.$emit('swipeend');
+        }
+
+        attachPinchEvent(touch1: Touch, touch2: Touch) {
+            this.pinchEvent = new MomijiPinchEvent(touch1, touch2);
+            this.$emit('pinchstart', this.pinchEvent);
+        }
+
+        updatePinchEvent(touch1: Touch, touch2: Touch) {
+            if (!!this.pinchEvent) {
+                this.pinchEvent.move(touch1, touch2);
+                this.$emit('pinchmove', this.pinchEvent);
+            }
+        }
+
+        detachPinchEvent() {
+            this.pinchEvent = undefined;
+            this.$emit('pinchend');
         }
     };
 </script>
