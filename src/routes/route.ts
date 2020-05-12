8c5c951e4af95a0df6d257f5c0ac4b580c8da9ea
おかもと <az.kinosaki@gmail.com>
2020-05-12 16:39:26 +0900

    [update]廃止するつもりだったものを廃止

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,7 +1,5 @@
 import Preview from "~/src/pages/Preview.vue";
-import SwipeableBoxPreview from "~/src/pages/SwipeableBoxPreview.vue";
 import FullscreenPreview from "~/src/pages/FullscreenPreview.vue";
-import MomijiPreview from "~/src/pages/MomijiPreview.vue";
 
 export default {
     routes: [
@@ -11,18 +9,9 @@ export default {
             name: 'base'
         },
         {
-            path: '/box-preview/:index',
-            name: 'box-preview',
-            component: SwipeableBoxPreview
-        },
-        {
             path: '/full',
             name: 'fullscreen',
             component: FullscreenPreview
-        }, {
-            path: '/momiji',
-            name: 'momiji',
-            component: MomijiPreview
-        }
+        },
     ]
 }
